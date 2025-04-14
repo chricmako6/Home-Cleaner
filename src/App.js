8454cfc8053d5872060abc1ec4bc691223242924
chricmako6 <156549010+chricmako6@users.noreply.github.com>
2025-04-14 19:34:05 +0300

    chnages for routing the pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Hero from "../src/Container/Hero";
-import About from "./Container/About";
-import Services from "./Container/Services"; 
-import Functionality from "./Container/Functionality";
-import Testimonial from "./Container/Testimonial";
-import Footer from "./Container/Footer"
+import Home from "./pages/Home";
 import ProfileCreation from "./pages/ProfileCreation"
 
 export default function App(){
   return(
     <Router>
       <Routes>
-        <Route path="/" element={
-          <div>
-            <Hero />
-            <About />
-            <Services />
-            <Functionality />
-            <Testimonial />
-            <Footer />
-          </div>
-        } />
+        <Route path="/" element={<Home/>} />
         <Route path="/profile" element={<ProfileCreation />} />
-        <Route path="/" element={<ProfileCreation />} />
       </Routes>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import Card from "../components/Card"
+import Hero from "../Container/Hero";
+import About from "../Container/About";
+import Services from "../Container/Services"; 
+import Functionality from "../Container/Functionality";
+import Testimonial from "../Container/Testimonial";
+import Footer from "../Container/Footer"
 
 export default function Home(){
-
     return(
-
         <>
-        <Card/>
+        <Hero />
+        <About />
+        <Services />
+        <Functionality />
+        <Testimonial />
+        <Footer />
         </>
     );
 }
